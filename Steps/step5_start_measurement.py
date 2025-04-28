cd3d308d731aad37bc492f73297ed0a0cc758197
Ango01 <aniagl@kth.se>
2025-04-28 13:41:30 +0200

    radial angles fix

--- Steps/step5_start_measurement.py
+++ Steps/step5_start_measurement.py
@@ -112,9 +112,9 @@ def save_bsdf(app):
         # Prepare angles
         incidence_angles = sorted(set(k[0] for k in app.bsdf_measurements.keys()))
         azimuth_angles = sorted(set(k[1] for k in app.bsdf_measurements.keys()))
+        # Get radial angles safely
         first_key = next(iter(app.bsdf_measurements.keys()))
-        first_radials = sorted(r for (r, *_means) in app.bsdf_measurements[first_key])
-        radial_angles = first_radials
+        radial_angles = sorted(set(r for (r, *_means) in app.bsdf_measurements[first_key]))
 
         # Group by (rotation, incidence)
         tis_data = {}
